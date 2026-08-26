097551cb6bcd778972ea307b17ba98c3710ade7c
sid6595 <ksmallareddygari@gmail.com>
2026-08-26 17:24:15 -0400

    Add: sql-common-table-expressions - submission-2

---
+++ SQL/sql-common-table-expressions/submission-2.sql
@@ -0,0 +1,55 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+WITH customer_orders AS (
+  SELECT customer_id, sum(price) as total_spend
+  FROM orders
+  GROUP BY customer_id
+  HAVING sum(price) < 100
+)
+
+SELECT name
+FROM customers
+JOIN customer_orders on customer_id = id;
+
+
+
+
+
+
+
+
+
+
+
+
+
