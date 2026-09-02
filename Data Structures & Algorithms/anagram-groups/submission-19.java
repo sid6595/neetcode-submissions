46deb757fe8265d14cec1edd8495eaebf75720ea
sid6595 <ksmallareddygari@gmail.com>
2026-09-02 00:02:34 -0400

    Add: anagram-groups - submission-19

---
+++ Data Structures & Algorithms/anagram-groups/submission-19.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
